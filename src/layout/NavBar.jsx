70cbf498d8318d0dfab4deb796e8a9467df7eb2f
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2024-03-11 16:14:00 +0700

    css-layout

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { PiStudent } from "react-icons/pi";
 const NavBar = () => {
     return (
-        <div className="nav-bar">
+        <div className="nav-bar-item">
             <div className="logo">
                 <PiStudent />
                 <p className="name-business">
